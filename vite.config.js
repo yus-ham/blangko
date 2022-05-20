15b114f3862dfe13e73d6e62657c76d330693008
Yusup Hambali <sup.alpuket@gmail.com>
2022-05-20 19:27:42 +0700

    support for env variable BASE_URL, API_URL

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
-import restApi from './src_api';
-import config from './config.env.json';
+import { globals, server } from './config.env.json';
+import restApi from './src_api/index.js';
 
 const __dir = _ => {
     try { return __dirname } catch(e) {}
     return import.meta.url.replace('file://', '').split('?')[0].replace('/vite.config.js', '')
 }
 
-Object.entries(config.globals||{}).forEach(([key, value]) => globalThis[key] = value)
+Object.entries(globals||{}).forEach(([key, value]) => globalThis[key] = value)
 
-if (process.env.PORT) {
-    config.server.port = process.env.PORT;
-}
+process.env.PORT && (server.port = process.env.PORT);
+process.env.BASE_URL && (globalsThis.BASE_URL = process.env.BASE_URL);
+process.env.API_URL && (globalThis.API_URL = process.env.API_URL);
 
 
 export default defineConfig({
-    server: config.server,
+    server,
 
     plugins: [
         restApi(),
