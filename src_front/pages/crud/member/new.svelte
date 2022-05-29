232ba75b8ad45477ca851fddf9ad2c0681c32c89
Yusup Hambali <sup.alpuket@gmail.com>
2022-05-30 06:36:48 +0700

    Fix error in edit page

---
+++ src_front/pages/crud/member/new.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { goto } from '@roxi/routify';
+  import Form from './_form.svelte';
+
+
+  let entityPath = '/crud/member'
+
+  wretch.addEventListener('success', e => {
+    if (e.detail.request.method === 'POST') {
+      $goto(`${e.detail.response.data.id}/edit`)
+    }
+  })
+</script>
+
+<Form title="Add New Member" method="post" action="{api(entityPath)}">
+  <div slot="bottom">
+    <a href="{entityPath}">Back</a>
+  </div>
+</Form>
