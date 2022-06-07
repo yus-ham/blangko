6e3395548e9d1e7f7f198ff3b82bbdffd760e9a8
Yusup Hambali <sup.alpuket@gmail.com>
2022-06-08 02:43:04 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
+import copy from 'rollup-plugin-copy';
 import restApi from './src_api/index.js';
 
 const __dir = _ => {
@@ -7,16 +8,14 @@ const __dir = _ => {
     return import.meta.url.replace('file://', '').split('?')[0].replace('/vite.config.js', '')
 }
 
-const globals = {
-    API_URL: '/api',
-    BASE_URL: '',
-}
-const server = {}
+const globals = {API_URL: '/api', BASE_URL: ''};
+const server = {};
 
 Object.entries(globals||{}).forEach(([key, value]) => globalThis[key] = value)
 
+
 process.env.PORT && (server.port = process.env.PORT);
-process.env.BASE_URL && (globalsThis.BASE_URL = process.env.BASE_URL);
+process.env.BASE_URL && (globalThis.BASE_URL = process.env.BASE_URL);
 process.env.API_URL && (globalThis.API_URL = process.env.API_URL);
 
 
@@ -26,11 +25,10 @@ export default defineConfig({
     plugins: [
         restApi(),
         svelte(),
+        copy({targets: [{src: 'src_api/*', dest: 'dist/functions/api'}]})
     ],
 
     resolve: {
-        alias: {
-            '~': __dir() +'/src_front'
-        }
+        alias: {'~': __dir() +'/src_front'}
     },
 });
