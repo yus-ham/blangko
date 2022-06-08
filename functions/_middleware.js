d300d8101c126e28e1451998cb047cd5163cc841
Yusup Hambali <sup.alpuket@gmail.com>
2022-06-09 01:31:21 +0700

    update

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -3,7 +3,7 @@ import cookie from 'cookie';
 
 async function parse({request}) {
   request.cookies = cookie.parse(request.headers.cookie||'') || {};
-  return next()
+  return await next()
 }
 
 export const onRequest = [parse]
