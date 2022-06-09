a6c468eb01d1e9ebb4a8eaf23496a38258c1b049
Yusup Hambali <sup.alpuket@gmail.com>
2022-06-09 20:37:22 +0700

    update

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,14 +1,26 @@
 import cookie from 'cookie';
 
+
+const parsers = {
+  'application/json': req => req.json(),
+  'multipart/form-data': req => req.formData(),
+  'application/x-www-form-urlencoded': parseQs,
+}
+
+function parseQs(req) {
+  return req.text(qs => {
+    qs = [...new URLSearchParams(qs)]
+    return qs.reduce((data, pair) => (data[pair[0]] = pair[1], data), {})
+  })
+}
+
 export const onRequest = [
   async function parse({request, next, data}) {
     data.cookies = await cookie.parse(request.headers.cookie||'') || {};
 
-    request.formUrl = function() {
-      return request.text(qs => {
-        qs = [...new URLSearchParams(qs)]
-        return qs.reduce((data, pair) => (data[pair[0]] = pair[1], data), {})
-      })
+    const type = request.headers.get('content-type')
+    if (type && parsers[type]) {
+      data.body = parsers[type](request)
     }
 
     return next()
