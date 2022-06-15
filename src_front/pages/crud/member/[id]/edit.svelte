76ab5a6370d9e474e8ad33149e51c80b2c5102dd
Yusup Hambali <sup.alpuket@gmail.com>
2022-06-15 14:38:57 +0700

    Show page not found on edit page

--- src_front/pages/crud/member/[id]/edit.svelte
+++ src_front/pages/crud/member/[id]/edit.svelte
@@ -1,6 +1,8 @@
 <script>
   import { params } from '@roxi/routify';
   import Form from '../_form.svelte';
+  import NotFounPage from '~/pages/_fallback.svelte';
+
 
   let entityPath = '/crud/member'
 </script>
@@ -12,4 +14,8 @@
       <a href="{entityPath}">List</a> | <a href="{entityPath}/new">Add New</a>
     </div>
   </Form>
+{#catch err}
+  {#if err.response.status === 404} <NotFounPage />
+  {#else} {console.error(err)||''}
+  {#endif}
 {#endawait}
