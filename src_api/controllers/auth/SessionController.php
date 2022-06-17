2a77ba71a8462c2ea13dc39c6b3a5f0af3d5b76c
Yusup Hambali <sup.alpuket@gmail.com>
2022-06-17 12:14:01 +0700

    [BE] cleanup..

--- src_api/controllers/auth/SessionController.php
+++ src_api/controllers/auth/SessionController.php
@@ -3,23 +3,9 @@
 namespace app\controllers\auth;
 
 use Yii;
-use app\models\auth\LoginForm;
-use app\models\auth\User;
 
 class SessionController extends \yii\rest\ActiveController
 {
     public $modelClass = 'app\models\auth\Session';
     public $skipAuth = '*';
-
-    // public function actionCreate()
-    // {
-    //     $model = new LoginForm();
-    //     $model->load(Yii::$app->request->bodyParams, '');
-
-    //     if ($model->login()) {
-    //         Yii::$app->response->statusCode = 201;
-    //     }
-
-    //     return $model;
-    // }
 }
