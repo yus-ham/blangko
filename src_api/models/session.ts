b930dab1de1b5e5f07f202005e44bf6acbdd923b
Yusup Hambali <sup.alpuket@gmail.com>
2022-09-24 18:13:40 +0700

    fix indentation

--- src_api/models/session.ts
+++ src_api/models/session.ts
@@ -7,46 +7,46 @@ import User from './user.ts';
 
 
 export default {
-  async loginByToken(rt = null) {
-    const result = await common.db().raw(`
-        select username,role.name as role from user
-        join refresh_token on user.id=user_id
-        join role on role.id=role_id
-        where value=? limit 1`,
-    [rt])
+    async loginByToken(rt = null) {
+        const result = await common.db().raw(`
+            select username,role.name as role from user
+            join refresh_token on user.id=user_id
+            join role on role.id=role_id
+            where value=? limit 1`,
+        [rt])
 
-    if (result[0]) {
-        return {
-            token: Buffer.from(config.jwtKey + Date.now()).toString('base64'),
-            identity: { username: result[0].username, role: result[0].role }
+        if (result[0]) {
+            return {
+                token: Buffer.from(config.jwtKey + Date.now()).toString('base64'),
+                identity: { username: result[0].username, role: result[0].role }
+            }
         }
-    }
-  },
+    },
 
-  async loginByPassword(info) {
-    const errors = { password: `Username or password invalid.` }
+    async loginByPassword(info) {
+        const errors = { password: `Username or password invalid.` }
         const identity = await User.findByUsername(info.username)
 
-    if (!identity) {
-        return common.invalidValueError(errors)
-    }
+        if (!identity) {
+            return common.invalidValueError(errors)
+        }
 
         const valid = Bcrypt.compareSync(info.password, '$2b$'+ identity.password_hash.slice(4))
-    if (!valid) {
-        return common.invalidValueError(errors)
-    }
+        if (!valid) {
+            return common.invalidValueError(errors)
+        }
 
         info.client_id = info.client_id
-    info.user_id = identity.id;
-    identity.role = identity.name;
-    delete identity.password_hash;
+        info.user_id = identity.id;
+        identity.role = identity.name;
+        delete identity.password_hash;
 
-    return RefreshToken.getOrCreate(info)
-        .then(rt => ({
-            identity,
-            token: Jwt.sign({ id: identity.id }, config.jwtKey),
-            refresh_token: rt.value,
-        }))
-        .catch(err => common.invalidValueError(errors))
-  }
+        return RefreshToken.getOrCreate(info)
+            .then(rt => ({
+                identity,
+                token: Jwt.sign({ id: identity.id }, config.jwtKey),
+                refresh_token: rt.value,
+            }))
+            .catch(err => common.invalidValueError(errors))
+    }
 }
