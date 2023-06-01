216730c8d660d46225d77d4d22c4c331e43d8888
Yusup Hambali <sup.alpuket@gmail.com>
2023-06-01 21:49:13 +0700

    Clear temp model before leaving update page

--- src_front/pages/crud/member/[id]/edit.svelte
+++ src_front/pages/crud/member/[id]/edit.svelte
@@ -1,6 +1,6 @@
 <script>
     import { redirectData } from '~/utils/store';
-    import { url, params } from '@roxi/routify';
+    import { afterUrlChange, url, params } from '@roxi/routify';
     import Form from '../_form.svelte';
     import NotFounPage from '~/pages/_fallback.svelte';
 
@@ -8,13 +8,18 @@
     let route = '/crud/member';
 
     const getModel = () => $redirectData.model
-               ? {data: $redirectData.model}
-               : api.fetch(`${route}/${$params.id}`)
+                 ? {data: $redirectData.model}
+                 : api.fetch(`${route}/${$params.id}`)
+
+
+    $afterUrlChange(_ => {
+        $redirectData.model = null;
+    })
 </script>
 
 
 {#await getModel()}Loading...{#then respon}
-    <Form title="Update Member" model="{respon.data}" method="patch" action="{api(`${route}/${$params.id}`)}">
+    <Form title="Update Member" model={respon.data} method="patch" action={api(`${route}/${$params.id}`)}>
         <div slot="bottom">
             <a href="{$url(route)}">List</a> | <a href="{$url(route)}/new">Add New</a>
         </div>
