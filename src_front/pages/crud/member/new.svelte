5e10575de2883ae6c2e08023b73dc8f2fd321e4b
Yusup Hambali <sup.alpuket@gmail.com>
2023-06-01 21:54:54 +0700

    Use `on:success` event in _form instead

--- src_front/pages/crud/member/new.svelte
+++ src_front/pages/crud/member/new.svelte
@@ -3,20 +3,16 @@
     import { redirectData } from "~/utils/store";
     import Form from "./_form.svelte";
 
+
     const route = "/crud/member";
 
-    wretch.addEventListener("success", (e) => {
-        if (
-            e.detail.request.method === "POST" &&
-            e.detail.request.url.includes(route)
-        ) {
-            $redirectData.model = e.detail.response.data;
-            $goto(`${route}/[id]/edit`, {id:e.detail.response.data.id});
-        }
-    });
+    function onSuccess({detail: model}) {
+        $redirectData.model = model;
+        $goto(`${route}/[id]/edit`, {id: model.id})
+    }
 </script>
 
-<Form title="Add New Member" method="post" action={api(route)}>
+<Form title="Add New Member" action={api(route)} on:success={onSuccess}>
     <div slot="bottom">
         <a href={$url(route)}>Back</a>
     </div>
