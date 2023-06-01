54c105db60f36676e4ffdbd2b5f6c20bf3ef1f89
Yusup Hambali <sup.alpuket@gmail.com>
2023-06-01 19:22:47 +0700

    Apply BASE_URL to all routes

--- src_front/pages/crud/member/[id]/edit.svelte
+++ src_front/pages/crud/member/[id]/edit.svelte
@@ -1,26 +1,25 @@
 <script>
-  import { redirectData } from '~/utils/store';
-  import { params, url } from '@roxi/routify';
-  import Form from '../_form.svelte';
-  import NotFounPage from '~/pages/_fallback.svelte';
+    import { redirectData } from '~/utils/store';
+    import { url, params } from '@roxi/routify';
+    import Form from '../_form.svelte';
+    import NotFounPage from '~/pages/_fallback.svelte';
 
 
-  let resource = '/crud/member';
+    let resource = '/crud/member';
 
-  const getModel = () => $redirectData.model
-                 ? {data: $redirectData.model}
-                 : api.fetch(`${resource}/${$params.id}`)
+    const getModel = () => $redirectData.model
+               ? {data: $redirectData.model}
+               : api.fetch(`${resource}/${$params.id}`)
 </script>
 
 
 {#await getModel()}Loading...{#then respon}
-  <Form title="Update Member" model="{respon.data}" method="patch" action="{api(`${resource}/${$params.id}`)}">
-    <div slot="bottom">
-      <a href="{resource}">List</a> | <a href="{resource}/new">Add New</a>
-    </div>
-  </Form>
+    <Form title="Update Member" model="{respon.data}" method="patch" action="{api(`${resource}/${$params.id}`)}">
+        <div slot="bottom">
+            <a href="{$url(resource)}">List</a> | <a href="{$url(resource)}/new">Add New</a>
+        </div>
+    </Form>
 {#catch err}
-  {#if err.response.status === 404} <NotFounPage />
-  {#else} {console.error(err)||''}
-  {#endif}
+    {#if err.response.status === 404} <NotFounPage />
+    {#else} {console.error(err)||''}{#endif}
 {#endawait}
